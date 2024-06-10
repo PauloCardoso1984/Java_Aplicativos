46a5e24f9d30b7b449dd7f996ec653141e0d2ebd
Paulinho <paulocardosoassessor@gmail.com>
2024-06-10 20:24:18 -0300

    AC Incremento Decremento

---
+++ Projetos/AC_Incremento_Decremento/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numero = 10;
+
+        // Incrementar
+        numero += 1;
+        System.out.println(numero);
+        System.out.println();
+
+        // Incrementar
+        numero++;
+        System.out.println(numero);
+        System.out.println();
+
+        // Incremento com FOR
+        for(int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+        System.out.println();
+
+        // Decremento com FOR
+        for(int i = 10; i >= 0; i--){
+            System.out.println(i);
+        }
+
+    }
+}
