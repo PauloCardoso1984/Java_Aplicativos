4a87d111d3fed37b7393a86517a11fee4f345440
Paulinho <paulocardosoassessor@gmail.com>
2024-06-15 16:55:25 -0300

    AJ_ Operadores de comparação e igualdade

---
+++ Projetos/AJ_Operadores_comparacao_igualdade/src/Operadores.java
@@ -0,0 +1,32 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        /*
+        * COMPARAÇÃO DE VALORES
+
+        * Maior         >
+        * Menor         <
+        * Menor igaual  <=
+        * Maior igual   >=
+
+        * COMPARADORES DE IGUALDADE
+        * Igual         ==
+        * Diferente     !=
+        * Negação       !
+
+        * USADO PARA AVALIAR A RELAÇÃO ENTRE VALORES
+        * Conjunção (AND)       &&
+        * Disjunção (OR)        ||
+
+        * PRIORIDADE MATEMÁTICP
+        * Parenteses    ()
+        * Negação       !
+        * Conjunção     &&
+        * Disjunção     ||
+        *
+        * Exemplo -> if (a && b || c && d) {}
+
+        * 
+        * */
+    }
+}
